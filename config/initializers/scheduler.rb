1a3592da7267fd2a72ad8ed3ec1b1e4b6a64f407
Martin Brugger <martin@brugger.eu>
2015-06-14 10:10:31 +0200

    Use environment variable SCHEDULE_VALIDATION_TASKS to enable/disable scheduling of validation tasks

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,18 +6,19 @@ require 'rufus-scheduler'
 # Let's use the rufus-scheduler singleton
 #
 s = Rufus::Scheduler.singleton
+if ENV['SCHEDULE_VALIDATION_TASKS']
+  s.cron '30 1 * * *' do
+    Rails.logger.info "Running working day validation at #{Time.now}"
+    WorkingDayValidationController.validate_previous_working_day
+  end
 
-s.cron '30 1 * * *' do
-  Rails.logger.info "Running working day validation at #{Time.now}"
-  WorkingDayValidationController.validate_previous_working_day
-end
+  s.cron '30 1 * * 5' do
+    Rails.logger.info "Running working week validation at #{Time.now}"
+    WorkingDayValidationController.validate_previous_week
+  end
 
-s.cron '30 1 * * 5' do
-  Rails.logger.info "Running working week validation at #{Time.now}"
-  WorkingDayValidationController.validate_previous_week
-end
-
-s.cron '30 1 1 * *' do
-  Rails.logger.info "Running working month validation at #{Time.now}"
-  WorkingDayValidationController.validate_previous_month
+  s.cron '30 1 1 * *' do
+    Rails.logger.info "Running working month validation at #{Time.now}"
+    WorkingDayValidationController.validate_previous_month
+  end
 end
