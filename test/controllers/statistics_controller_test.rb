3363e0ae1aa61a6fe5390b238cf72572ff0b99f8
Martin Brugger <martin@brugger.eu>
2015-07-25 20:05:06 +0200

    fixed scaffolded statistics tests, added basic test checking authorization and basic leave days calculation

--- test/controllers/statistics_controller_test.rb
+++ test/controllers/statistics_controller_test.rb
@@ -1,19 +1,44 @@
 require 'test_helper'
 
 class StatisticsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should get index" do
+    given_authenticated_user(users(:admin_user))
     get :index
     assert_response :success
   end
 
   test "should get working_hours" do
+    given_authenticated_user(users(:admin_user))
     get :working_hours
     assert_response :success
   end
 
   test "should get leave_days" do
+    given_authenticated_user(users(:admin_user))
     get :leave_days
     assert_response :success
+    assert_not_nil assigns(:user_leave_days_statistic)
+    assert_equal 254, assigns(:total_leave_days)
+  end
+
+  test "user should not be allowed to get index" do
+    given_authenticated_user(users(:user))
+    get :index
+    assert_response :redirect
+  end
+
+  test "user should not be allowed to get working_hours" do
+    given_authenticated_user(users(:user))
+    get :working_hours
+    assert_response :redirect
+  end
+
+  test "user should not be allowed to get leave_days" do
+    given_authenticated_user(users(:user))
+    get :leave_days
+    assert_response :redirect
   end
 
 end
