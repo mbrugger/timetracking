a11ae8f7e639fc5759108fdecdda5d8a59cd4482
Martin Brugger <martin@brugger.eu>
2015-08-05 20:34:51 +0200

    moved generate token code to model

--- app/controllers/api/v1/api_login_controller.rb
+++ app/controllers/api/v1/api_login_controller.rb
@@ -8,17 +8,10 @@ class Api::V1::ApiLoginController < Api::V1::ApiApplicationController
 
   def login
     if current_user.token.nil?
-      current_user.token = generate_authentication_token
+      current_user.token = current_user.generate_authentication_token
       current_user.save
     end
     render json: { token: current_user.token }
-  end
-
-  def generate_authentication_token
-    loop do
-      token = Devise.friendly_token
-      break token unless User.where(token: token).first
-    end
   end
 
 end
