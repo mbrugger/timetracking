042b8ac4d9111ba30cdbea1dbcffb248af0faa21
Martin Brugger <martin@brugger.eu>
2015-08-15 14:24:04 +0200

    added basic contoller test verifying authentication of request

---
+++ test/controllers/api/api_calendars_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class ApiCalendarsControllerTest < ActionController::TestCase
+  tests Api::V1::ApiCalendarsController
+
+  test "should return unauthorized for missing authentication token" do
+    get :leave_days, format: :ics
+    assert_response :unauthorized
+  end
+
+  test "should return unauthorized for wrong authentication token" do
+    get :leave_days, format: :ics, API_AUTH_TOKEN: 'some_wrong_token'
+    assert_response :unauthorized
+  end
+
+  test "should get leave days with valid token" do
+    get :leave_days, format: :ics, API_AUTH_TOKEN: users(:working_api_user_with_pause).token
+    assert_response :success
+  end
+
+
+end
